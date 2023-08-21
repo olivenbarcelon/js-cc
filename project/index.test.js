4b8f52a6382457707850a610b17bfc0f4a52ba80
olivenbarcelon <olivenbarcelon@gmail.com>
2023-08-21 21:43:33 +0800

    optimized code

--- project/index.test.js
+++ project/index.test.js
@@ -1,5 +1,7 @@
 const sum = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('Index', function () {
+    it('It should add 1 + 2', function () {
+        expect(sum(1, 2)).toBe(3);
+    });
 });
